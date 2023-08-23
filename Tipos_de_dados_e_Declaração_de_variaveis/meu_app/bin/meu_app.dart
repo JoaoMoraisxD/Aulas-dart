adfb611d0df277e44146d25dac8596e1b5481f7c
JoaoMoraisxD <joaomoraisxd@gmailcom>
2023-08-23 08:12:15 -0300

    Fit: Criando projeto para estudar e compreender melhor tipo de dados e declaração de variaveis com dart

---
+++ Tipos_de_dados_e_Declaração_de_variaveis/meu_app/bin/meu_app.dart
@@ -0,0 +1,18 @@
+import 'package:meu_app/meu_app.dart' as meu_app;
+
+void main(List<String> arguments) {
+  String texto = "DIO";
+  var texto2 = "DIO";
+  String text3;
+
+  int numero1 = 1;
+  var numero2 = 2;
+  int numero3;
+
+  List<String> lista = [];
+  lista.add("A");
+  var lista1 = [];
+  lista1.add("A");
+  lista1.add(1);
+  lista1.add(1.2);
+}
---
+++ meu-primeiro-app/bin/meu_primeiro_app.dart
@@ -0,0 +1,5 @@
+import 'package:meu_primeiro_app/meu_primeiro_app.dart' as meu_primeiro_app;
+
+void main(List<String> arguments) {
+  print('Hello world: ${meu_primeiro_app.calculate()}!');
+}
