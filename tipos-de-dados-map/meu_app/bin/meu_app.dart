d92d81743bce6c71c41126cb4b20061d75fa50c8
JoaoMoraisxD <joaomoraisxd@gmailcom>
2023-08-25 18:10:56 -0300

    aula tipos de dados map

---
+++ tipos-de-dados-map/meu_app/bin/meu_app.dart
@@ -0,0 +1,9 @@
+import 'package:meu_app/meu_app.dart' as meu_app;
+
+void main(List<String> arguments) {
+  Map<String, dynamic> map1 = Map<String, dynamic>();
+  var map = {'zero': 0, 'one': 1, 'two': 2};
+
+  print(map1);
+  print(map);
+}
